6b59ff092ecd5e84586817c50f5682c0a39b10d8
barroz1996 <62878759+barroz1996@users.noreply.github.com>
2024-08-31 23:22:21 +0300

    added search

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -57,4 +57,21 @@ const authUser = asyncHandler(async(req,res) => {
     throw new Error("Invalid Email or Password")
   }
 });
-module.exports = { registerUser, authUser };
+
+
+
+// /api/user
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search ? {
+    $or: [
+      {name : { $regex: req.query.search, $options: "i"}},
+      {email: { $regex: req.query.search, $options: "i"}},
+    ],
+  }: {};
+
+  const users = await User.find(keyword); //excpet the user logged in return everyother user with   the conditions
+  res.send(users); 
+  
+});
+
+module.exports = { registerUser, authUser, allUsers };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes")
+const chatRoutes = require("./routes/chatRoutes")
+
 const {notFound,errorHandler} = require('./middleware/errorMiddleware')
 
 
@@ -17,7 +19,8 @@ app.get('/', (req,res)=>{
   res.send("API is running")
 })
 
-app.use('/api/user',userRoutes)
+app.use('/api/user',userRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.use(notFound)
 app.use(errorHandler)
